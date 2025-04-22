449a0e7d97fd349d0881f2dca55b94be1b5f366d
Nicolas Georges <nicolas.georges@creatis.insa-lyon.fr>
2025-04-22 17:51:46 +0200

    app import: PoC create pipeline

--- vip-api/src/main/java/fr/insalyon/creatis/vip/api/controller/processing/PipelineController.java
+++ vip-api/src/main/java/fr/insalyon/creatis/vip/api/controller/processing/PipelineController.java
@@ -35,19 +35,29 @@ import fr.insalyon.creatis.vip.api.business.PipelineBusiness;
 import fr.insalyon.creatis.vip.api.controller.ApiController;
 import fr.insalyon.creatis.vip.api.exception.ApiException;
 import fr.insalyon.creatis.vip.api.model.Pipeline;
+import fr.insalyon.creatis.vip.application.client.bean.AppVersion;
 import fr.insalyon.creatis.vip.application.client.bean.Application;
+import fr.insalyon.creatis.vip.application.server.business.AppVersionBusiness;
+import fr.insalyon.creatis.vip.application.server.business.ApplicationBusiness;
 import fr.insalyon.creatis.vip.application.server.model.boutiques.BoutiquesDescriptor;
+import fr.insalyon.creatis.vip.core.client.bean.User;
+import fr.insalyon.creatis.vip.core.server.business.BusinessException;
+import fr.insalyon.creatis.vip.core.server.business.Server;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * Created by abonnet on 7/28/16.
@@ -60,10 +70,19 @@ public class PipelineController extends ApiController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final PipelineBusiness pipelineBusiness;
+    private final Supplier<User> currentUserProvider;
+    private final ApplicationBusiness applicationBusiness;
+    private final AppVersionBusiness appVersionBusiness;
 
     @Autowired
-    protected PipelineController(PipelineBusiness pipelineBusiness) {
+    protected PipelineController(PipelineBusiness pipelineBusiness,
+                                 Supplier<User> currentUserProvider,
+                                 ApplicationBusiness applicationBusiness,
+                                 AppVersionBusiness appVersionBusiness) {
         this.pipelineBusiness = pipelineBusiness;
+        this.currentUserProvider = currentUserProvider;
+        this.applicationBusiness = applicationBusiness;
+        this.appVersionBusiness = appVersionBusiness;
     }
 
     @RequestMapping
@@ -123,4 +142,48 @@ public class PipelineController extends ApiController {
     public Pipeline getPipelineWithRequestParam(@RequestParam String pipelineId) throws ApiException {
         return getPipeline(pipelineId);
     }
+
+    @Autowired Server server;
+    @RequestMapping(method = RequestMethod.POST)
+    public Pipeline createPipeline(@RequestBody @Valid Pipeline pipeline)
+            throws ApiException {
+        logMethodInvocation(logger, "createPipeline", pipeline);
+        // XXX pipelineBusiness.listPipelines("aa");
+        //String execId = executionBusiness.initExecution(execution);
+        //return executionBusiness.getExecution(execId, false);
+        //return new Pipeline("test/1.2.3","test","1.2.3");
+        //ApplicationImporterServiceAsync s = ApplicationImporterService.Util.getInstance();
+        //BoutiquesApplication bt = null;
+        //s.createApplication(bt, false, [], [], null);
+        //ApplicationImporterBusiness a = Bean.getApplicationImporterBusiness.class;
+
+        // see ApplicationImporterBusiness.java:registerApplicationVersion()
+        try {
+            String rootFolder = server.getApplicationImporterRootFolder();
+            String appName = pipeline.getName();
+            String appVersion = pipeline.getVersion();
+
+            Application app = applicationBusiness.getApplication(appName);
+            if (app == null) {
+                applicationBusiness.add(new Application(appName, "")); // currentUserProvider.get().getEmail()
+            }
+            AppVersion newVersion = new AppVersion(appName, appVersion,
+                    rootFolder + "/" + appName + "_" + appVersion + ".json",
+                    true, true);
+            // appVersionBusiness.update(newVersion);
+            List<AppVersion> versions = appVersionBusiness.getVersions(appName);
+            for (AppVersion existingVersion : versions) {
+                if (existingVersion.getVersion().equals(newVersion.getVersion())) {
+                    appVersionBusiness.update(newVersion);
+                    return pipeline;
+                }
+            }
+            appVersionBusiness.add(newVersion);
+            return pipeline;
+        } catch (BusinessException e) {
+            logger.info("XXX createPipeline error: ",e);
+            return pipeline;
+        }
+    }
+
 }
