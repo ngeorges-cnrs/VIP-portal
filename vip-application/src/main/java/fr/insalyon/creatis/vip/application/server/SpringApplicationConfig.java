44078506930e115b4089946fe192b1fbc3fd0863
Nicolas Georges <nicolas.georges@creatis.insa-lyon.fr>
2025-05-06 08:42:21 +0200

    Remove moteur_service_wsdl and Soap client

--- vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/SpringApplicationConfig.java
+++ vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/SpringApplicationConfig.java
@@ -3,7 +3,6 @@ package fr.insalyon.creatis.vip.application.server;
 import fr.insalyon.creatis.moteur.plugins.workflowsdb.WorkflowsDBException;
 import fr.insalyon.creatis.moteur.plugins.workflowsdb.dao.*;
 import fr.insalyon.creatis.vip.application.server.business.simulation.RestServiceEngine;
-import fr.insalyon.creatis.vip.application.server.business.simulation.SoapServiceEngine;
 import fr.insalyon.creatis.vip.application.server.business.simulation.WorkflowEngineInstantiator;
 import fr.insalyon.creatis.vip.core.server.business.Server;
 import org.hibernate.SessionFactory;
@@ -64,7 +63,7 @@ public class SpringApplicationConfig {
         if (server.useRestMoteurServer()) {
             return new RestServiceEngine(server);
         }
-        return new SoapServiceEngine();
+        return null; // XXX
     }
 
 }
