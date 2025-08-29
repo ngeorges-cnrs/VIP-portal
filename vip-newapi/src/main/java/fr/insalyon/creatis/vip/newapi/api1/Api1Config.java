bd7a08314686d7d8014926ddd6385a6ddf9692b5
Nicolas Georges <nicolas.georges@creatis.insa-lyon.fr>
2025-08-29 17:08:36 +0200

    Experimental multi-api
    Variant 2: ComponentScan to exclude all RestController components from
    the root context, and include them in their respective context.
    Behaviour: /api1 and /api2 are correctly isolated, but all controllers
    are available under /rest.
    
    /rest/platform: 200 (normal)
    /rest/test1: 200 (should be 404)
    /rest/test2: 200 (should be 404)
    
    /api1/platform: 404 (normal)
    /api1/test1: 200 (normal)
    /api1/test2: 404 (normal)
    
    /api2/platform: 404 (normal)
    /api2/test1: 404 (normal)
    /api2/test2: 200 (normal)

--- vip-newapi/src/main/java/fr/insalyon/creatis/vip/newapi/api1/Api1Config.java
+++ vip-newapi/src/main/java/fr/insalyon/creatis/vip/newapi/api1/Api1Config.java
@@ -2,8 +2,12 @@ package fr.insalyon.creatis.vip.newapi.api1;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(includeFilters = @ComponentScan.Filter(type=FilterType.ANNOTATION, value=RestController.class))
 public class Api1Config {
 }
